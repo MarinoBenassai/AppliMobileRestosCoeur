106770273a52b2aaea0840693b1d7eb4131e99c8
benassai <benassai@etud.insa-toulouse.fr>
2021-06-17 15:49:02 +0200

    Navigation entre les différents écrans via bouton

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StyleSheet, Button, Text, View, Image, TextInput} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import IdScreen from './src/screens/Identification';
 import compteScreen from './src/screens/Compte';
@@ -9,7 +11,12 @@ import contactScreen from './src/screens/Contacts';
 import engagementScreen from './src/screens/Engagements';
 import referentScreen from './src/screens/SynthRef';
 
-const Stack = createStackNavigator();
+const engagementStack = createStackNavigator();
+const synthRefStack = createStackNavigator();
+const compteStack = createStackNavigator();
+const contactsStack = createStackNavigator();
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   const [userID,setUserID] = React.useState("");
@@ -21,28 +28,69 @@ export default function App() {
   
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Drawer.Navigator screenOptions = {{swipeEnabled : false}}>
 	  {userID === "" ? (
-	    <Stack.Screen name="Identification">
+	    <Drawer.Screen name="Identification">
 		  {(props) => (
             <IdScreen onSignIn={check_password} />
           )}
-		</Stack.Screen>
+		</Drawer.Screen>
 	  ) : (
 		<>
-    <Stack.Screen name="Contacts" component={contactScreen} options={{ title: "Mes Contacts" }} />
-	  <Stack.Screen name="Engagements" component={engagementScreen} options={{ title: "Liste des engagements" }} />
-		<Stack.Screen name="SynthRef" component={referentScreen} options={{ title: "Synthèse Référent" }} />
-		<Stack.Screen name="Compte" component={compteScreen} options={{ title: "Mon Compte" }} />
-		
-
-	    </>
+		  <Drawer.Screen name="Engagements" component={engagement} options={{ title: "Mes engagements" }} />
+		  <Drawer.Screen name="SynthRef" component={referent} options={{ title: "Ma synthèse référent" }} />
+		  <Drawer.Screen name="Compte" component={compte} options={{ title: "Mon compte" }} />
+		  <Drawer.Screen name="Contacts" component={contact} options={{ title: "Mes contacts" }} />
+		</>
 	  )}
-	  </Stack.Navigator>
+	  </Drawer.Navigator>
     </NavigationContainer>
   );
 }
 
+const boutonMenu = ({nav}) => {
+	return (<Button
+              onPress={nav.openDrawer}
+              title="Menu"
+              color="#080"
+            />)
+}
+
+function engagement({navigation}) {
+  const nav = navigation
+  return (
+    <engagementStack.Navigator screenOptions={{headerLeft: () => (boutonMenu({nav})),}}>
+	  <engagementStack.Screen name="Engagements" component={engagementScreen} options={{title: "Mes engagements"}}/>
+    </engagementStack.Navigator>
+  );
+}
+
+function referent({navigation}) {
+  const nav = navigation
+  return (
+    <engagementStack.Navigator screenOptions={{headerLeft: () => (boutonMenu({nav})),}}>
+	  <synthRefStack.Screen name="SynthRef" component={referentScreen} options={{ title: "Ma synthèse référent" }} />
+    </engagementStack.Navigator>
+  );
+}
+
+function compte({navigation}) {
+  const nav = navigation
+  return (
+    <engagementStack.Navigator screenOptions={{headerLeft: () => (boutonMenu({nav})),}}>
+	  <compteStack.Screen name="Compte" component={compteScreen} options={{ title: "Mon compte" }} />
+    </engagementStack.Navigator>
+  );
+}
+
+function contact({navigation}) {
+  const nav = navigation
+  return (
+    <engagementStack.Navigator screenOptions={{headerLeft: () => (boutonMenu({nav})),}}>
+	  <contactsStack.Screen name="Contacts" component={contactScreen} options={{ title: "Mes contacts" }} />
+    </engagementStack.Navigator>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
