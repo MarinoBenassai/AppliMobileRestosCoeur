60630d5c40e0e1c8d9a7bbf1a200b9c8c6e3d92e
Bastien <bast17@orange.fr>
2021-06-18 16:58:39 +0200

    debut activité

--- src/screens/Activite/index.js
+++ src/screens/Activite/index.js
@@ -1,24 +1,95 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Text, View} from 'react-native';
-import {SafeAreaView, StyleSheet, StatusBar, Pressable, Alert} from 'react-native';
-import {Linking} from 'react-native';
+import {SafeAreaView, StyleSheet, StatusBar, Pressable} from 'react-native';
 
 import userContext from '../../contexts/userContext';
 
-const activiteScreen = ({route}) => {
+// Fonction Principale
+function activiteScreen({route}) {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState('');
+  
+  // On charge l'id de l'utilisateur courrant
   const userID = React.useContext(userContext).userID
 
+  // On récupère les informations données en paramètres
   const { IDActivite, IDSite, IDJour } = route.params;
 
-  return (
-    <Text style = {styles.container}>
-		{"Ceci est l'ecran des activites. IDActivite=" + IDActivite + ", IDJour= " + IDJour + ", IDSite=" + IDSite}
-    </Text>
+
+  // On va chercher les données
+  useEffect(() => {
+    fetch('http://51.38.186.216/Axoptim.php/REQ/AP_LST_PRE_EQU/P_IDBENEVOLE=' + userID + '/P_IDACTIVITE=' + IDActivite + '/P_IDSITE=' + IDSite + '/P_JOUR=' + IDJour)
+      .then((response) => response.text())
+      .then((texte) =>  {setData(texte); console.log({texte})})
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  // On traite les données
+  const ligne = data.split(/\n/);
+  ligne.shift(); //enlève le premier élement (et le retourne)
+  ligne.pop();   //enlève le dernier élement (et le retourne)
+
+  // On crée le renderer pour la flatlist
+  const renderItem = ({ item }) => (
+    // Conteneur Principal de chaque item
+    <View style={[styles.item, styles[item.split(/\t/)[3]]]}>
+
+      {/* Conteneur 1ere colonne */}
+      <Pressable onPress={() => versActivite({navigation})} >
+      {({ pressed }) => (
+        <View style={{flexDirection: "column",}}>
+          <Text style={{color: pressed ? 'white' : 'black',}}>{item.split(/\t/)[2]}</Text>
+          <Text style={{color: pressed ? 'white' : 'black',}}>{item.split(/\t/)[1]}</Text> 
+          <Text style={{color: pressed ? 'white' : 'black',}}>
+            {item.split(/\t/)[0].split(" ")[0].split("-")[2]}/
+            {item.split(/\t/)[0].split(" ")[0].split("-")[1]}/
+            {item.split(/\t/)[0].split(" ")[0].split("-")[0]}
+          </Text>
+          <Text style={{color: pressed ? 'white' : 'black',}}>Participants : {item.split(/\t/)[6]}</Text>
+        </View> )}
+      </Pressable>
+
+      {/* Conteneur 2eme colonne (modifiable : status + commentaire)*/}
+      <View style={{ flexDirection: "column"}}>
+        <Pressable onPressOut={() => changerStatut(item.split(/\t/)[4])}>
+          <Text style={{ color: (item.split(/\t/)[4] == "Absent") ? 'black' : ((item.split(/\t/)[4] == "Present") ? "green" : "red") }}>{item.split(/\t/)[4]}</Text>
+        </Pressable>
+        
+        <Text>{item.split(/\t/)[5]}</Text>
+      </View>
+    </View>
   );
 
+  // On retourne la flatliste
+  return (
+    <>
+    <SafeAreaView style={styles.container}>
+    {isLoading ? (
+      <View style={styles.loading}>
+        <ActivityIndicator size="large" color="#00ff00" />
+      </View>) : (
+      <FlatList
+        data={ligne}
+        renderItem={renderItem}
+        keyExtractor={item => item}
+      />
+      )}
+    </SafeAreaView>
+	</>
+  );
 }
 
 
+// Fonction de changement de statut
+const changerStatut = (statut) => {
+  console.log("changerStatut : " + statut);
+}
+
+
+
+// Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -26,20 +97,31 @@ const styles = StyleSheet.create({
   },
   item: {
     flexDirection: "row",
-    backgroundColor: '#00ffff',
+    justifyContent:"space-evenly",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
-    borderBottomRightRadius: 50,
-    borderBottomLeftRadius: 50,
   },
-  title: {
-    fontSize: 32,
+
+  // Couleur dynamique
+  BENEVOLE: {
+    backgroundColor: '#6fe3d2',
   },
-}); 
+  REFERENT: {
+    backgroundColor: '#f9c2ff',
+  },
+  loading: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+	  backgroundColor: '#F5FCFF88',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 
-
+// On exporte la fonction principale
 export default activiteScreen;
