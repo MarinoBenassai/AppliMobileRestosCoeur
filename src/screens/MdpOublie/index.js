9e07ee272610f3be1c62b085f8fe125f84fe0e2c
benassai <benassai@etud.insa-toulouse.fr>
2021-06-22 11:47:36 +0200

    Ajout de la requête d'oubli de mot de passe

--- src/screens/MdpOublie/index.js
+++ src/screens/MdpOublie/index.js
@@ -3,6 +3,9 @@ import { ActivityIndicator, FlatList, Text, View} from 'react-native';
 import {SafeAreaView, StyleSheet, TextInput, Button, StatusBar, Pressable, Alert} from 'react-native';
 
 import constantes from '../../constantes';
+import * as Crypto from 'expo-crypto';
+import * as Random from 'expo-random';
+
 
 function oublieScreen({navigation}){
   const [textEmail, onChangeTextEmail] = React.useState('');
@@ -28,12 +31,21 @@ function oublieScreen({navigation}){
 	if (data != "" && tempEmail != "") {
 	  const ID = getIDFromData(data);
 	  if (ID != null) {
-	    alert("Un mail contenant votre nouveau mot de passe vient d'être envoyé à " + tempEmail);
+		const nouveauMDP = "rdc-" + getRandomWord(5);
+		Crypto.digestStringAsync(
+			Crypto.CryptoDigestAlgorithm.SHA256,
+			nouveauMDP
+		  )
+		.then((hash1) => fetch('http://' + constantes.BDD + '/Axoptim.php/REQ/AP_UPD_MOTDEPASSE/P_IDBENEVOLE=' + ID + '/P_MOTDEPASSE=' + hash1))
+		.then((rep) => rep.text())
+		.then(texte => {if (texte != "1\n") {throw new Error("Erreur lors de la miseà jour de la base de données");}})
+		.catch();
+	    alert("Un mail contenant votre nouveau mot de passe vient d'être envoyé à " + tempEmail +". Votre nouveau mot de passe est " + nouveauMDP +".");
 		navigation.goBack();
 	  }
 	
 	  else {
-	    alert("Adresse email incorrecte");
+	    alert("Adresse email incorrecte.");
 	  }
 	  setData("");
 	  setTempEmail("");
@@ -78,6 +90,19 @@ function oublieScreen({navigation}){
     </>
   );
 
+}
+
+function getRandomWord(nbChar){
+	var bytes = [];
+	while (bytes.length < nbChar){
+		const byt = Random.getRandomBytes(1)[0];
+		if ((byt > 47 && byt < 58) || (byt > 64 && byt < 91) || (byt > 96 && byt < 123)){
+			bytes.push(byt);
+		}
+	}		
+	const word = bytes.map((elem) => String.fromCharCode(elem));
+	return word.join("");
+	
 }
 
 const styles = StyleSheet.create({
