e2ab7a8eddd6eedbe574dbd2f4ec20e6e0d22cd4
benassai <benassai@etud.insa-toulouse.fr>
2021-07-26 12:15:58 +0200

    checkFetch attend un JSON

--- src/components/checkFetch.js
+++ src/components/checkFetch.js
@@ -3,13 +3,13 @@ import {Alert} from 'react-native';
 
 export const  checkFetch = async function(response) {
     // console.info("response : " + response + "\n" + response.ok + "\n" + response.status);
-	const texte = await response.text();
+	const json = await response.json();
 	
     if(response.ok){
-        return texte;
+        return json;
     }
     else{
-		throw texte;
+		throw json.error;
         /*alert("Il y a eu un problème lors de la connexion à la page. Veuillez rééssayer.");
         console.error("Erreur Fetch : " + response.text());
         return "-1";*/
